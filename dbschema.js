606563ed4f74361f7b90dbebf038ff06c8f72dc5
Arbiona <63557021+arb1ona@users.noreply.github.com>
2020-06-28 17:06:49 +0200

    .files added

---
+++ dbschema.js
@@ -0,0 +1,12 @@
+let db = {
+
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream/post body',
+            createdAt: '2020-06-27T21:15:45.285Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
